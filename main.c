e3e406f4954b6164426b016bfc3eefe9c1fb9dc8
ectos0130 <ectos0130@naver.com>
2023-11-02 16:30:23 +0900

    03_1

--- main.c
+++ main.c
@@ -4,18 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum = 0;
-	for(i=0;i<SIZE;i++){
-		printf("학생 성적을 입력하세요: ");
-		scanf("%d", &grade[i]);
-		sum+=grade[i]; 
-	}
-	average = sum/SIZE;
-	printf("성적 평균:%d\n",average);
-	
+	for(i=0;i<SIZE;i++)
+		grade[i] = rand()%100+1;
+	for(i=0;i<SIZE;i++)	
+		score[i] = grade[i];
+	for(i=0;i<SIZE;i++)
+		printf("score[%d] = %d (grade : %i)\n", i, score[i], grade[i]);
+		
 	return 0;
 }
